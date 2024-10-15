bae52e76ee77a4e0dfd10f2233bb49b8c5e5fc16
Sairaj Padghe <padghe.jarias@gmail.com>
2024-10-15 19:04:09 +0530

    basic stats logic added

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,11 +1,18 @@
-using System;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-
-    public Stat m_Strength;
+    [Header("Major Stats")] 
+    public Stat m_Strength;         // 1 point increases damage by 1 and crit power by 1%
+    public Stat m_Agility;          // 1 point increases evasion by 1% and crit chance by 1%
+    public Stat m_Intelligence;     // 1 point increases magic damage by 1 and magic resistance by 3
+    public Stat m_Vitality;         // 1 point increases health by 3 or 5 points
+    
+    [Header("Defensive Stats")]
     public Stat m_MaxHealth;
+    public Stat m_Armor;
+    public Stat m_Evasion;
+    
     public Stat m_Damage;
 
     private int m_CurrentHealth;
@@ -20,12 +27,32 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void CalculateAndDoDamage(CharacterStats _targetStats)
     {
-
-
-
+        if (CanEvade(_targetStats)) return;
+        
         int totalDamage = m_Damage.Value + m_Strength.Value;
         
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
+        
         _targetStats.TakeDamage(totalDamage);
+    }
+
+    private static int CheckTargetArmor(CharacterStats _targetStats, int _totalDamage)
+    {
+        _totalDamage -= _targetStats.m_Armor.Value;
+        _totalDamage = Mathf.Max(_totalDamage, 0);
+        return _totalDamage;
+    }
+
+    private static bool CanEvade(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.m_Evasion.Value + _targetStats.m_Agility.Value;
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            Debug.Log("Damage avoided");
+            return true;
+        }
+        return false;
     }
 
     public virtual void TakeDamage(int _damage)
